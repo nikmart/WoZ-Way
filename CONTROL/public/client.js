3316f3352c096eb75cc2c597e899b068a6bfe5d3
CDR DesignX <cdrdesignx@DN0a23820f.SUNet>
2017-02-11 14:56:34 -0800

    add CONTROL station code with CAN speed data

---
+++ CONTROL/public/client.js
@@ -0,0 +1,151 @@
+// JavaScript Document
+var socket = io();
+
+function sendOnEnter(){
+	// send on enter key
+	if (event.keyCode == 13){
+		sendMsg();
+	}
+}
+
+function sendMsg(){
+	// get and send the messge to the remote interface
+	var msg = document.getElementById("message").value;
+	console.log(msg);
+	socket.emit('msg', msg);  //send the message to ther server
+
+	// add the question to the list
+	addQuestion(msg);
+
+	// reset the message window
+	resetMsg();
+	}
+
+function resetMsg(){
+	document.getElementById("message").value = '';
+	}
+
+function addQuestion(msg){
+	// create a new line with the questions at the top of the list
+	var para = document.createElement("p");
+	var node = document.createTextNode(msg);
+	para.appendChild(node);
+
+	var btn = document.createElement("BUTTON");
+	var btnReplay = document.createTextNode("Replay"); // Create a text node
+	btn.onclick = function(){replayMsg(msg)};
+	btn.appendChild(btnReplay);
+
+	para.appendChild(btn);
+	var element = document.getElementById("questions");
+	element.appendChild(para);
+}
+
+function replayMsg(msg){
+	console.log(msg);
+	socket.emit('msg', msg);  //send the message to ther server
+}
+
+function playMsg(msgID){
+	var msg = document.getElementById(msgID).innerHTML;
+	console.log(msg);
+	socket.emit('msg', msg);  //send the message to ther server
+}
+
+function readEnglish(){
+	lang = 'english';
+  socket.emit('lang', lang);
+}
+
+function readFrench(){
+	lang = 'french';
+  socket.emit('lang', lang);
+}
+
+function readJapanese(){
+	lang = 'japanese'
+  socket.emit('lang', lang);
+}
+
+// read the data from the message that the server sent and change the
+// display based on the message
+// message should have a name, value, and date
+socket.on('can-msg', function(msg) {
+	canData = JSON.parse(msg);
+	console.log(canData.name);
+	switch (canData.name) {
+		case 'vss': //vehicle speed
+			document.getElementById('CAN').innerHTML = canData.value;
+			break;
+		default:
+			break;
+	}
+});
+
+    //switch(json_msg.name) {
+        // case 'CAN':
+        //     console.log('speed: ', json_msg.value)
+				// 	var speed = document.getElementById('CAN');
+				// 	//This is the corerction factor for the Q50
+				// 	correctedSpeed = Math.ceil(json_msg.value * 0.84);
+    		// 		speed.innerHTML = correctedSpeed;
+        //     break;
+
+    //     case 'Status':
+    //         console.log('status: ', json_msg.value);
+		// 				var status = document.getElementById('ACCStatus');
+		// 				status.innerHTML = json_msg.value;
+		// 				// change the background color of the cell
+		// 				if (json_msg.value == 'On') {
+		// 					status.style.backgroundColor = "#00cc66"; //green
+		// 				}
+		// 				else {
+		// 					status.style.backgroundColor = "#f8f8f8"; //grey
+		// 				}
+    //         break;
+		//
+		// case 'Brake':
+		// 		console.log('brake: ', json_msg.value);
+		// 		var brake = document.getElementById('Brake')
+		// 		brake.innerHTML = json_msg.value;
+		// 		// change the background color of the cell
+		// 		if (json_msg.value == 1) {
+		// 			brake.style.backgroundColor = "#ffff66"; //yellow
+		// 		}
+		// 		else {
+		// 			brake.style.backgroundColor = "#f8f8f8"; //grey
+		// 		}
+		// 	  break;
+		//
+		// case 'Distance':
+		// 		console.log('distance: ', json_msg.value);
+		// 		var dist = document.getElementById('ICCDistance');
+		// 		dist.innerHTML = json_msg.value;
+		// 		break;
+		//
+		// case 'Indicator':
+		// 		console.log('indicator: ', json_msg.value);
+		// 		var engaged = document.getElementById('ICCEngaged');
+		// 		//Change the color and say if engaged or not
+		// 		if (json_msg.value){
+		// 			engaged.innerHTML = "Engaged";
+		// 			engaged.style.backgroundColor = "#00cc66"; //green
+		// 		}
+		// 		else {
+		// 			engaged.innerHTML = "-";
+		// 			engaged.style.backgroundColor = "#f8f8f8"; //grey
+		// 		}
+		// 		break;
+		//
+		// case 'Speed2': //ICC set speed
+		// 		console.log('set speed: ', json_msg.value);
+		// 		var set = document.getElementById('ICCSetSpeed');
+		// 		set.innerHTML = json_msg.value - 1; //there is an offset of 1 on this speed
+		// 		break;
+
+		// case 'can': //ICC above set speed, usually means driver accelerating
+		// 		console.log('above speed: ', json_msg.value);
+		// 		var set = document.getElementById('CAN');
+		// 		set.innerHTML = json_msg.value;
+		// 		break;
+    // }
---
+++ CONTROL/server.js
@@ -0,0 +1,73 @@
+var express = require('express'); // web server application
+var http = require('http');				// http basics
+var app = express();							// instantiate express server
+var server = http.Server(app);		// connects http library to server
+var io = require('socket.io')(server);	// connect websocket library to server
+var serverPort = 8000;
+var mqtt    = require('mqtt');
+var client  = mqtt.connect('mqtt://hri.stanford.edu',
+                           {port: 8134,
+                            protocolId: 'MQIsdp',
+                            protocolVersion: 3 });
+
+// use express to create the simple webapp
+app.use(express.static('public'));		// find pages in public directory
+
+
+// Setup the MQTT connection and listen for messages
+client.on('connect', function () {
+  //Subscribe to topics
+  client.subscribe('say');
+  client.subscribe('can');  //CAN data in JSON
+  console.log("Waiting for messages...");
+  client.publish('say', 'Hello, I am a need finding machine');
+});
+
+// process the MQTT messages
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(topic, message.toString());
+
+  switch (topic) {
+    case 'can':
+      io.emit('can-msg', message.toString());
+      break;
+    case 'gps':
+      io.emit('gps-msg', message.toString());
+    default:
+      io.emit('server-msg', message.toString());
+
+  }
+  //client.end();
+});
+
+
+// this is the websocket event handler and say if someone connects
+// as long as someone is connected, listen for messages
+io.on('connect', function(socket) {
+    console.log('a user connected');
+
+    // if you get a message to send, send to the MQTT broker
+    socket.on('msg', function(msg) {
+        console.log(language + ', ' + msg);
+        //send it to the mqtt broker
+        client.publish(language, msg);
+    });
+
+    //change the language
+    socket.on('lang', function(lang) {
+      console.log(lang);
+      language = lang;
+    });
+
+    // if you get the 'disconnect' message, say the user disconnected
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
+});
+
+
+// start the server and say what port it is on
+server.listen(serverPort, function() {
+    console.log('listening on *:%s', serverPort);
+});
